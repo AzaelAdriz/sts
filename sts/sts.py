6792bbc386d1166fb75816630d860e036aba159c
Chris Evardo <christopher.evardo@ngc.com>
2019-08-29 11:13:56 -0400

    Add exception handling for connection failures

--- sts/sts.py
+++ sts/sts.py
@@ -15,38 +15,45 @@ os.system("echo *** Running STS Credential Script ***")
 tokenCode = str(sys.argv[1])
 os.system("echo Token Code: " + tokenCode)
 
-# Execute AWS CLI command to retrieve STS token
-command = os.system('aws sts get-session-token --serial-number ' +
-                    arn + ' --profile initial --token-code ' + tokenCode + ' 1>out.txt')
+try:
+    # Execute AWS CLI command to retrieve STS token
+    command = os.system('aws sts get-session-token --serial-number ' +
+                        arn + ' --profile initial --token-code ' + tokenCode + ' 1>out.txt')
 
-# Store generated keys
-tempJsonFile = 'out.txt'
-with open(tempJsonFile) as json_file:
-    data = json.load(json_file)
-print(data)
+    # Store generated keys
+    tempJsonFile = 'out.txt'
+    with open(tempJsonFile) as json_file:
+        data = json.load(json_file)
+    print(data)
 
-# Create or replace credentials file
-with open(credentials_file_path, 'w') as credentialsFile:
-    content = [
-        "[default]",
-        "aws_access_key_id = " + data['Credentials']['AccessKeyId'],
-        "aws_secret_access_key = " + data['Credentials']['SecretAccessKey'],
-        "aws_session_token = " + data['Credentials']['SessionToken'],
-        "[initial]",
-        "aws_access_key_id = " + initial_aws_access_key_id,
-        "aws_secret_access_key = " + initial_aws_secret_access_key
-    ]
-    credentialsFile.writelines("%s\n" % line for line in content)
-    credentialsFile.close()
+    # Create or replace credentials file
+    with open(credentials_file_path, 'w') as credentialsFile:
+        content = [
+            "[default]",
+            "aws_access_key_id = " + data['Credentials']['AccessKeyId'],
+            "aws_secret_access_key = " +
+            data['Credentials']['SecretAccessKey'],
+            "aws_session_token = " + data['Credentials']['SessionToken'],
+            "[initial]",
+            "aws_access_key_id = " + initial_aws_access_key_id,
+            "aws_secret_access_key = " + initial_aws_secret_access_key
+        ]
+        credentialsFile.writelines("%s\n" % line for line in content)
+        credentialsFile.close()
 
-print("Credentials file updated!")
-print("Cleaning up...")
+    print("Credentials file updated!")
+except Exception as err:
+    print("STS Token generation failed!\nMeet the following prerequisites to proceed:\n  - Connect to cloudvpn.cms.gov\n  - Have BIC AWS permissions granted\n  - Entered the correct MFA token code\n")
+    print(err)
 
-# Clean up
-if os.path.exists(tempJsonFile):
-    json_file.close()
-    os.remove(tempJsonFile)
-else:
-    print("The file does not exist")
+finally:
+    print("Cleaning up...")
 
-print("Complete!")
+    # Clean up
+    if os.path.exists(tempJsonFile):
+        json_file.close()
+        os.remove(tempJsonFile)
+    else:
+        print("The file does not exist")
+
+    print("Complete!")
